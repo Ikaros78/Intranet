e5d4b1b371fb1950e3271b7ea359253401a4b4ed
Ikaros78 <mydowson@gmail.com>
2022-08-29 18:07:47 +0900

    설정 추가, 결재부분 초기설정 추가

--- src/main/java/com/ohgiraffers/intranet/config/IntranetApplication.java
+++ src/main/java/com/ohgiraffers/intranet/config/IntranetApplication.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.intranet;
+package com.ohgiraffers.intranet.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

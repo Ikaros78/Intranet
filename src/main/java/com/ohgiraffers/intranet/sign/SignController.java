e4b75a91bc0e5e83163c761642213d01cb80606a
Ikaros78 <mydowson@gmail.com>
2022-08-31 10:40:00 +0900

    최신화를 위한 commit

--- src/main/java/com/ohgiraffers/intranet/sign/SignController.java
+++ src/main/java/com/ohgiraffers/intranet/sign/SignController.java
@@ -1,9 +1,31 @@
 package com.ohgiraffers.intranet.sign;
 
+import com.ohgiraffers.intranet.sign.model.service.SignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/sign")
+@RequestMapping("/sign/*")
 public class SignController {
+
+    private final SignService signService;
+
+    @Autowired
+    public SignController(SignService signService){
+
+        this.signService = signService;
+    }
+
+    @GetMapping(value = {"/main", "/waitingList"})
+    public ModelAndView signWaitingList(ModelAndView mv){
+
+
+
+        mv.setViewName("sign/waitingList");
+
+        return mv;
+    }
 }

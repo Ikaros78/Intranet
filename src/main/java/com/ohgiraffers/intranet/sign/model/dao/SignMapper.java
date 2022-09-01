77ca3ec88d0501acbf5a979a70bcba087c45f71e
Ikaros78 <mydowson@gmail.com>
2022-09-01 12:36:10 +0900

    결재대기함 작업중

--- src/main/java/com/ohgiraffers/intranet/sign/model/dao/SignMapper.java
+++ src/main/java/com/ohgiraffers/intranet/sign/model/dao/SignMapper.java
@@ -1,7 +1,15 @@
 package com.ohgiraffers.intranet.sign.model.dao;
 
+import com.ohgiraffers.intranet.common.paging.SelectCriteria;
+import com.ohgiraffers.intranet.sign.model.dto.SignDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SignMapper {
+    int selectTotalWaitingCount(Map<String, String> searchMap);
+
+    List<SignDTO> selectWaitingList(SelectCriteria selectCriteria);
 }

755aa557c00493f8836dbf486b7b84115a716d02
JongCheol_Choi <zxcvzxcv0103@gmail.com>
2022-09-01 14:57:00 +0900

    캘린더 select문 시작을 위한 최종 커밋

--- src/main/java/com/ohgiraffers/intranet/calendar/controller/CalendarController.java
+++ src/main/java/com/ohgiraffers/intranet/calendar/controller/CalendarController.java
@@ -1,8 +1,19 @@
 package com.ohgiraffers.intranet.calendar.controller;
 
+import com.ohgiraffers.intranet.calendar.model.service.CalendarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cd")
 public class CalendarController {
+
+    @GetMapping("/cdmain")
+    public String Cdmain() {
+
+        return "calendar/cd_main";
+    }
 
 }

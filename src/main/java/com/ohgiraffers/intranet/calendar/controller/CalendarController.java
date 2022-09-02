f5096e0bbd316175f97c85391efbc13379057555
JongCheol_Choi <zxcvzxcv0103@gmail.com>
2022-09-02 10:19:28 +0900

    09-02 최신화를 위한 커밋 오전 10시

--- src/main/java/com/ohgiraffers/intranet/calendar/controller/CalendarController.java
+++ src/main/java/com/ohgiraffers/intranet/calendar/controller/CalendarController.java
@@ -1,19 +1,62 @@
 package com.ohgiraffers.intranet.calendar.controller;
 
+import com.ohgiraffers.intranet.calendar.model.dto.CalendarDTO;
 import com.ohgiraffers.intranet.calendar.model.service.CalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
-@RequestMapping("/cd")
+@RequestMapping("/calendar")
 public class CalendarController {
 
+    private final CalendarService calendarService;
+
+    @Autowired
+    public CalendarController(CalendarService calendarService){
+
+        this.calendarService = calendarService;
+    }
+    
     @GetMapping("/cdmain")
     public String Cdmain() {
 
         return "calendar/cd_main";
     }
+    @GetMapping("/all")
+    public ModelAndView findScList(ModelAndView mv) {
+
+        List<CalendarDTO> calList = calendarService.findAllSc();
+
+        mv.addObject("calList", calList);
+        mv.setViewName("/cdmain/all");
+
+        return mv;
+    }
+
+    /* 전체 조회용 ajax를 위한 메소드 */
+    @GetMapping(value="findAll", produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public List<CalendarDTO> findAllList() {
+
+        return calendarService.findAllCal();
+    }
+//
+//    /* DB 전체 조회용 */
+//    @GetMapping(value="cdAllMain", produces = "application/json; charset=UTF-8")
+//    @RequestBody // 비동기처리
+//    public List<CalendarDTO> selectAllCalendar() {
+//
+//        System.out.println("calendarService = " + calendarService);
+//        return calendarService.selectAllCalendar();
+//    }
 
 }
--- src/main/java/com/ohgiraffers/intranet/calendar/model/service/CalendarService.java
+++ src/main/java/com/ohgiraffers/intranet/calendar/model/service/CalendarService.java
@@ -1,6 +1,36 @@
 package com.ohgiraffers.intranet.calendar.model.service;
 
+import com.ohgiraffers.intranet.calendar.model.dao.CalendarMapper;
+import com.ohgiraffers.intranet.calendar.model.dto.CalendarDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CalendarService {
-/* 서비스 확인용 */
-/* 서비스 확인용 */
+
+    private CalendarMapper calendarMapper;
+
+    @Autowired
+    public CalendarService(CalendarMapper calendarMapper) {
+
+        this.calendarMapper = calendarMapper;
+    }
+
+//    /* DB 캘린더 전체 조회 */
+//    public List<CalendarDTO> selectAllCalendar() {
+//
+//        return calendarMapper.selectAllCalendar();
+//    }
+
+    public List<CalendarDTO> findAllCal() {
+
+        return calendarMapper.findAllCal();
+    }
+
+    public List<CalendarDTO> findAllSc() {
+
+        return calendarMapper.findAllSc();
+    }
 }

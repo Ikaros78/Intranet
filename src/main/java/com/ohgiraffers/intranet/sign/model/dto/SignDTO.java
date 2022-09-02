5dbddcfe9d44ed26e27b15275b27becbda551134
Ikaros78 <mydowson@gmail.com>
2022-09-02 02:10:23 +0000

    log4j 적용, yml 수정 결재대기함 진행중

--- src/main/java/com/ohgiraffers/intranet/sign/model/dto/SignDTO.java
+++ src/main/java/com/ohgiraffers/intranet/sign/model/dto/SignDTO.java
@@ -1,6 +1,9 @@
 package com.ohgiraffers.intranet.sign.model.dto;
 
+import com.ohgiraffers.intranet.member.model.dto.MemberDTO;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,9 +13,16 @@ public class SignDTO {
 
     private int signNo;
     private int signWriter;
+    private String signForm;
     private String signTitle;
     private String signContent;
     private java.sql.Date regDate;
     private String isTemp;
+
+    private MemberDTO writer;
+    private List<ApproverDTO> approver;
+    private List<ReceiverDTO> receiver;
+    private List<ReferencerDTO> referencer;
+    private List<SignFileDTO> fileList;
 
 }

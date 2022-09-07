8dea79be3bd66f418facf4ec47ed2b72a446cffa
Yu hee chang <yhc10215@gmail.com>
2022-09-07 05:41:05 +0000

    22-09-07 14:36 게시판 별 접근 권한 설정

--- src/main/java/com/ohgiraffers/intranet/main/controller/MainController.java
+++ src/main/java/com/ohgiraffers/intranet/main/controller/MainController.java
@@ -3,7 +3,10 @@ package com.ohgiraffers.intranet.main.controller;
 import com.ohgiraffers.intranet.main.model.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,4 +36,13 @@ public class MainController {
 
         return mv;
     }
+
+    /* userImpl을 통해 로그인 시 세션에 들고온 해당 계정의 정보를 들고 오는 방법입니다. */
+    @GetMapping(value = {"/", "/main"})
+    public String main(@AuthenticationPrincipal User user){
+
+        System.out.println("user : " + user);
+        return "main/main";
+    }
+
 }

cce3c732df419f3c856649917983e17459f7a76a
Ikaros78 <mydowson@gmail.com>
2022-09-16 11:22:42 +0900

    결재DTO 전체 lombok 제거

--- src/main/java/com/ohgiraffers/intranet/sign/model/dto/SignFormDTO.java
+++ src/main/java/com/ohgiraffers/intranet/sign/model/dto/SignFormDTO.java
@@ -1,17 +1,39 @@
 package com.ohgiraffers.intranet.sign.model.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Setter
-@Getter
-@ToString
-@RequiredArgsConstructor
 public class SignFormDTO {
 
     private String code;
     private String name;
 
+    public SignFormDTO() {
+    }
+
+    public SignFormDTO(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "SignFormDTO{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
